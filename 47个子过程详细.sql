967959d348f5141c45916562779c86a663db0612
linyushan <1107012776@qq.com>
2022-05-08 21:52:26 +0800

    47个子过程

--- 47个子过程详细.sql
+++ 47个子过程详细.sql
@@ -3,7 +3,7 @@ CREATE TABLE `itto_child` (
   `level_group` tinyint(4) NOT NULL DEFAULT '0' COMMENT '1输入 2工具 3结果',
   `sort` tinyint(4) NOT NULL DEFAULT '0' COMMENT '排序',
   `name` varchar(255) NOT NULL DEFAULT '' COMMENT '名称',
-  `subprocess_name` varchar(255) NOT NULL DEFAULT '' COMMENT '',
+  `subprocess_name` varchar(255) NOT NULL DEFAULT '' COMMENT '子过程名称',
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
